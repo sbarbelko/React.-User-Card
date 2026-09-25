f669f0a9f2af93ee4160ce2834db1c55a29b5b95
sbarbelko <s.barbelko63@gmail.com>
2026-09-25 22:57:59 +0300

    Add style

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,28 +1,28 @@
-import Footer from "../Footer";
-import Header from "../Header";
 import styles from "./UserCard.module.css";
+import profile from "./profile.jpg";
 
 function UserCard(props) {
   const { name, surname, nickname, sex, isVerified, stats } = props.userInfo;
-
-  const headerInfo = {
-    nameInf: name,
-    surnameInf: surname,
-    nicknameInf: nickname,
-  };
+  const { tweets, followers, following } = stats;
 
   return (
     <article className={styles.userCard}>
-      <Header headerInfo={headerInfo} />
-      <p>
-        Emma Watson is a British actress and activist who rose to global fame
-        playing Hermione Granger in the Harry Potter film series. Beyond her
-        iconic debut, she starred in hit films such as Beauty and the Beast and
-        Little Women, earned an English literature degree from Brown University,
-        and serves as a UN Women Goodwill Ambassador leading gender equality
-        initiatives like the HeForShe campaign.
-      </p>
-      <Footer stats={stats} />
+      <header>
+        <img src={profile} alt={name} />
+        <h1>{`${name} ${surname}`}</h1>
+        <span>{nickname}</span>
+      </header>
+      <footer>
+        <h2>
+          Tweets <span>{tweets}</span>
+        </h2>
+        <h2>
+          Followers <span>{followers}</span>
+        </h2>
+        <h2>
+          Following <span>{following}</span>
+        </h2>
+      </footer>
     </article>
   );
 }
